ac9d0bc7de526bf2c19714a4750941e84cb65d4a
Carlos Cabral <carlosfc.631@gmail.com>
2020-12-03 18:23:19 -0300

    Home casi terminado

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { LoginComponent } from './user/login/login.component';
+import { RegisterComponent } from './user/register/register.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from './content/home/home.component';
+import { SliderComponent } from './components/slider/slider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    LoginComponent,
+    RegisterComponent,
+    FooterComponent,
+    HomeComponent,
+    SliderComponent
   ],
   imports: [
     BrowserModule,
