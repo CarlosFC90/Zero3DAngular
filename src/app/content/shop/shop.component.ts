51f93697c1bf8e7fe8d838c38cb937e955a3d1bf
Carlos Cabral <carlosfc.631@gmail.com>
2020-12-29 15:31:06 -0300

    Todos los modals funcionando correctamente

--- src/app/content/shop/shop.component.ts
+++ src/app/content/shop/shop.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoService } from "../../services/info.service";
+import { OwlOptions } from 'ngx-owl-carousel-o';
+import { NgbModal } from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: 'app-shop',
@@ -7,9 +10,41 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ShopComponent implements OnInit {
 
-  constructor() { }
+  constructor(public _is: InfoService, public modal: NgbModal) { }
 
   ngOnInit(): void {
+  }
+
+  openLg(ModalPicap){
+    this.modal.open(ModalPicap, { size: 'lg'});
+  }
+
+
+  customOptions: OwlOptions = {
+    loop: true,
+    autoplay: true,
+    autoplayTimeout: 3000,
+    mouseDrag: true,
+    touchDrag: false,
+    pullDrag: false,
+    dots: true,
+    navSpeed: 1000,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 3
+      },
+      400: {
+        items: 3
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 3
+      }
+    },
+    nav: false
   }
 
 }
